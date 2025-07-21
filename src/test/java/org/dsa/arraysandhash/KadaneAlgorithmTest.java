2fce55bbf62367f279189f69d7a576d605916cf1
schoudhury <schoudhury@twilio.com>
2025-07-21 13:34:52 +0530

    Add parameterised tests

--- src/test/java/org/dsa/arraysandhash/KadaneAlgorithmTest.java
+++ src/test/java/org/dsa/arraysandhash/KadaneAlgorithmTest.java
@@ -1,30 +1,29 @@
 package org.dsa.arraysandhash;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class KadaneAlgorithmTest {
 
-    @Test
-    void testMaxSubArraySum() {
-        KadaneAlgorithm kadane = new KadaneAlgorithm();
-        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        assertEquals(6, kadane.maxSubArray(nums));
+    static Stream<Arguments> provideTestCases() {
+        return Stream.of(
+                Arguments.of(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}, 6),
+                Arguments.of(new int[]{-2, -3, -1}, -1),
+                Arguments.of(new int[]{5}, 5),
+                Arguments.of(new int[]{1, 2, 3, -2, 5}, 9),
+                Arguments.of(new int[]{-1, -2, -3}, -1)
+        );
     }
 
-    @Test
-    void testMaxSubArraySumWithAllNegative() {
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    void testMaxSubArray(int[] nums, int expected) {
         KadaneAlgorithm kadane = new KadaneAlgorithm();
-        int[] nums = {-2, -3, -1};
-        assertEquals(-1, kadane.maxSubArray(nums));
+        assertEquals(expected, kadane.maxSubArray(nums));
     }
-
-    @Test
-    void testMaxSubArraySumWithSingleElement() {
-        KadaneAlgorithm kadane = new KadaneAlgorithm();
-        int[] nums = {5};
-        assertEquals(5, kadane.maxSubArray(nums));
-    }
-
 }
